51f172c0b27f5cc092d2c170fb93d50fc735c57b
Matt Lee <mrlee@uchicago.edu>
2012-07-06 21:37:24 -0500

    solved problem 34 in Haskell

---
+++ 034/Prob34.hs
@@ -0,0 +1,15 @@
+import Data.Char
+
+--takes an integer and gives an array of digits
+numToDigits :: Int -> [Int]
+numToDigits n = map ((\x -> x - 48) . ord) $ show n
+
+fact :: Int -> Int
+fact n = product [1..n]
+
+-- takes an integer and returns the sum of the digits in factorial
+sumFact :: Int -> Int
+sumFact n = foldl (+) 0 $ map fact $ numToDigits n
+
+solver :: Int -> Int
+solver n = sum $ filter (\x -> x == sumFact x) [4..n]
